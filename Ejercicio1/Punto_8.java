0e97d73e2687983ec12b97643284d3cd790c0b32
Felipessss <jfelipej.04@gmail.com>
2026-09-13 02:23:43 +0000

    creacion arreglo de 30 posiciones, tambien un maximo y un minimo

--- Ejercicio1/Punto_8.java
+++ Ejercicio1/Punto_8.java
@@ -2,4 +2,16 @@ package Ejercicio1;
 
 public class Punto_8 {
     
+    public static void main(String[] args) {
+        
+        int[] numeros = new int [30];
+
+        int max = 100;
+        int min = 1;
+        
+
+
+    }
+
+
 }
