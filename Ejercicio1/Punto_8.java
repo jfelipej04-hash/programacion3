bd9c75b2bedb1a4eb70431088706974e6da60f13
Felipessss <jfelipej.04@gmail.com>
2026-09-13 02:31:56 +0000

    uso del for para identificar cual es el numero mayor y menor recorriendo todos los nmmeros

--- Ejercicio1/Punto_8.java
+++ Ejercicio1/Punto_8.java
@@ -8,10 +8,26 @@ public class Punto_8 {
 
         int max = 100;
         int min = 1;
-        
 
+         for (int i = 0; i < numeros.length; i++){
+        numeros[i] = (int)(Math.random() * (max - min + 1) + min);
+        System.out.println("numeros[" + i + "] = " + numeros[i]);
 
     }
+
+    int mayor = numeros [0];
+    int menor = numeros [0];
+
+    for (int i = 1; i < numeros.length; i++){
+        if (numeros[i] > mayor){
+            mayor = numeros[i];
+        }
+        if (numeros[i] < menor){
+            menor = numeros[i];
+        }
+    }
+
+    
 
 
 }
