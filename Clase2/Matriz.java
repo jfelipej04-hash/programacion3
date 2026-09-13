d3d2181494ed6ead9c61fa7d4f9641e6bd91c21d
Felipessss <jfelipej.04@gmail.com>
2026-09-13 00:33:30 +0000

    .

--- Clase2/Matriz.java
+++ Clase2/Matriz.java
@@ -1,22 +1,27 @@
 package Clase2;
 
-public class Matriz {
-    public static void main(String[] args) {
-        
-        //Declaracion de la matriz (forma 1)
-        int[][] m = {{5, 8, 6},
-                    {1, 3, 2},
-                     {4, 9, 7}     };
+public static void main(String[] args) {
 
-        //Recorrido de la matriz
+        // Declaracion de la matriz forma 1
+        int[][] a = { { 5, 8, 6 },
+                { 1, 3, 2 },
+                { 4, 7, 9 } };
 
-        for (int i = 0 ; i < m.length; i++){
-            for (int j = 0 ; j < m[0].length; j++){   
-               System.out.println("m[" + i + "][" + j + "]=" + m[i] [j]);
-            }   
-
-
+        // Recorrido de la matriz
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.println("a[" + i + "][" + j + "] = " + a[i][j]);
+            }
         }
 
+        // Visualizar de mejor manera la matriz
+        String cad = "";
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                cad += "|" + a[i][j] + "|";
+            }
+            cad += "\n";
+        }
+        System.out.println(cad);
     }
 }
---
+++ Ejercicio1/Punto_7.java
@@ -0,0 +1,9 @@
+package Ejercicio1;
+
+public class Punto_7 {
+
+    int [] A = {7, 2, 4, 19, 22, 8, 14, 25, 9, 1}
+
+    int contPares = 0, contImpares = 0;
+    
+}
