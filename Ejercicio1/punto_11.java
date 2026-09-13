cf36ec94a085fd56887f627e4108e42838c341be
Felipessss <jfelipej.04@gmail.com>
2026-09-13 04:41:33 +0000

    imprimir y correciones

--- Ejercicio1/punto_11.java
+++ Ejercicio1/punto_11.java
@@ -3,7 +3,7 @@ package Ejercicio1;
 public class punto_11 {
     public static void main(String[] args) {
         
-        int[] A = {3, -4, 0, -8, 7, 9, 0, -1, 5, -3}
+        int[] A = {3, -4, 0, -8, 7, 9, 0, -1, 5, -3};
 
         int[] positivos = new int[A.length];
         int[] negativos = new int[A.length];
@@ -25,8 +25,19 @@ public class punto_11 {
 
         }
 
-        
+        System.out.println("\nPositivos: ");
+        for (int i = 0; i < p; i++){
+            System.out.println(positivos[i]);
+        }
+         System.out.println("Negativos: ");
+        for (int i = 0; i < n; i++){
+            System.out.println(negativos[i]);
+        }
 
+         System.out.println("ceros: ");
+        for (int i = 0; i < c; i++){
+            System.out.println(ceros[i]);
+        }
 
     }
     
