98e06571830fa9f6913b7012c50cf153a5074d5f
Felipessss <jfelipej.04@gmail.com>
2026-09-13 04:14:02 +0000

    se crea el for para que se sumen los opuestos

--- Ejercicio1/Punto_10.java
+++ Ejercicio1/Punto_10.java
@@ -7,7 +7,11 @@ public class Punto_10 {
         int[] A = {7, 3, 6, 9, 1, 5, 4};
         int[] B = new int[4];
 
-        
+        for(int i = 0; i < B.length; i++) {
+            B[i] = A[i] + A[A.length -1 - i];
+        }        
+
+
 
     }
     
