eb621b42df24a9109535fbefa70afea610101606
Felipessss <jfelipej.04@gmail.com>
2026-09-13 03:36:49 +0000

    se importa el scanner que ayudara a leer los datos que digite el usuario

---
+++ Ejercicio1/Punto_9.java
@@ -0,0 +1,19 @@
+package Ejercicio1;
+
+import java.util.Scanner;
+
+public class Punto_9 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
+        
+
+
+
+
+    }
+
+    
+}
