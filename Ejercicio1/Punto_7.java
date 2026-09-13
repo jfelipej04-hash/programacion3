703fc0d566b646ef7e712f13fc4e88803b5af6aa
Felipessss <jfelipej.04@gmail.com>
2026-09-13 00:45:26 +0000

    recorrido con for y condicion con if-else

--- Ejercicio1/Punto_7.java
+++ Ejercicio1/Punto_7.java
@@ -5,5 +5,16 @@ public class Punto_7 {
     int [] A = {7, 2, 4, 19, 22, 8, 14, 25, 9, 1}
 
     int contPares = 0, contImpares = 0;
+
+    for (int num : A ){
+
+        if (num % 2 == 0){
+            contPares++;
+        } else {
+            contImpares++;
+        }
+
+
+    }
     
 }
