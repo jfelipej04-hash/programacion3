508e0075abd2e2c2965c9ba5df4235b57f1e1c3b
Felipessss <jfelipej.04@gmail.com>
2026-09-13 04:08:33 +0000

    se crea los arreglos el A que es el q va a sumar extremos y el B donde quedaran los resultados

---
+++ Ejercicio1/Punto_10.java
@@ -0,0 +1,14 @@
+package Ejercicio1;
+
+public class Punto_10 {
+
+    public static void main(String[] args) {
+        
+        int[] A = {7, 3, 6, 9, 1, 5, 4};
+        int[] B = new int[4];
+
+        
+
+    }
+    
+}
