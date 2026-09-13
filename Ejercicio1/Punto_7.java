5ca3d42bd0d5017bd9fcc11819e23b21e9f4ef0e
Felipessss <jfelipej.04@gmail.com>
2026-09-13 01:56:56 +0000

    creacion de los imprimir

--- Ejercicio1/Punto_7.java
+++ Ejercicio1/Punto_7.java
@@ -14,10 +14,41 @@ public class Punto_7 {
             contImpares++;
         }
 
+    }
+
     int[] pares = new int [contPares];
     int[] impares = new int [contImpares];
 
-    
+    int indP = 0, indIm = 0;
+
+    for (int num : A) {
+
+        if (num % 2 == 0) {
+            pares[indP++] = num;
+        } else {
+        impares[indIm++] = num;
+        
+        }
+    }
+
+    System.out.println("Arreglo original:");
+    for (int i = 0; i < A.length; i++){
+        System.out.println(A[i] + " ");
+    }
+
+    System.out.println("Arreglo original:");
+    for (int i = 0; i < pares.length; i++){
+        System.out.println(pares[i] + " ");
+    }
+
+    System.out.println("Arreglo original:");
+    for (int i = 0; i < impares.length; i++){
+        System.out.println(impares[i] + " ");
+    }
+
+
+
+
 
 
     }
