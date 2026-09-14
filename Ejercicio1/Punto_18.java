cf137ca14cea8d7e8a1589ff7fff8b1ac58d751e
Felipessss <jfelipej.04@gmail.com>
2026-09-14 02:56:30 +0000

    se crea el for que recorrrera toda la matriz multiplicando cada dato de fila por columna

--- Ejercicio1/Punto_18.java
+++ Ejercicio1/Punto_18.java
@@ -4,8 +4,12 @@ public class Punto_18 {
     public static void main(String[] args) {
         
         int [][] matriz = new int [10][10];
-        
 
+for(int i = 0; i < 10; i++){
+    for(int j = 0; j < 10; j++) {
+        matriz[i][j] = (i+1) * (j+1);
+    }
+}
 
 
 
