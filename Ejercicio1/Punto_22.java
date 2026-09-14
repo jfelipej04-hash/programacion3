329e2cf1ade844dea17ec0df8b8b78fa1db5fbaa
Felipessss <jfelipej.04@gmail.com>
2026-09-14 17:59:33 +0000

    pedimos elementos de la matriz y luego se muestra la matriz

--- Ejercicio1/Punto_22.java
+++ Ejercicio1/Punto_22.java
@@ -15,6 +15,26 @@ public class Punto_22 {
 
         int[][] matriz = new int[n][m];
 
+        System.out.println("\nIngrese los elementos de la matriz: ");
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                System.out.print("Elemento [" + i + "][" + j + "]: ");
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+         System.out.println("\nMatriz ingresada:");
+        for(int i = 0; i < n; i++){
+            System.out.print("[");
+            for(int j = 0; j < m; j++){
+                System.out.print(matriz[i][j]);
+                if(j < m-1) System.out.print(", ");
+            }
+            System.out.println("]");
+        }
+
+        
+
 
     }
     
