c047bb20eb3901d686308c46889784258e229e96
Felipessss <jfelipej.04@gmail.com>
2026-09-14 03:50:43 +0000

    creamos una matriz con rango de 5, luego por medio de un for y un if else logramos establecer los 0 y unos

---
+++ Ejercicio1/Punto_19.java
@@ -0,0 +1,24 @@
+package Ejercicio1;
+
+public class Punto_19 {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 5;
+
+        int [][] matriz = new int[a][b];
+
+        for(int i = 0; i < a; i++){
+            for(int j = 0; j < b; j++){
+                if(i == j){
+                    matriz[i][j] = 1;
+                }else {
+                    matriz[i][j] = 0;
+                }
+            }
+        }
+
+    }
+    
+}
