b5f1da7591d034ccaea75e444678e27046ecafa7
Felipessss <jfelipej.04@gmail.com>
2026-09-14 02:54:17 +0000

    se crea una matriz con el new int de tamaño 10x10

---
+++ Ejercicio1/Punto_18.java
@@ -0,0 +1,14 @@
+package Ejercicio1;
+
+public class Punto_18 {
+    public static void main(String[] args) {
+        
+        int [][] matriz = new int [10][10];
+        
+
+
+
+
+    }
+    
+}
