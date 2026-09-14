e19df0615f157fba6bd32aeee7a996d2e291f992
Felipessss <jfelipej.04@gmail.com>
2026-09-14 17:53:39 +0000

    usamos scanner, se pide columnas y filas luego se crea una matriz con esos datos

---
+++ Ejercicio1/Punto_22.java
@@ -0,0 +1,21 @@
+package Ejercicio1;
+
+import java.util.Scanner;
+
+public class Punto_22 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("ingrese el numero de filas: ");
+        int n = sc.nextInt();
+        System.out.println("ingrese el numero de columnas: ");
+        int m = sc.nextInt();
+
+        int[][] matriz = new int[n][m];
+
+
+    }
+    
+}
