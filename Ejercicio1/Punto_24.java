01dd1bb6806313d1816c17367b9e9828c1ec0de0
Felipessss <jfelipej.04@gmail.com>
2026-09-15 03:04:30 +0000

    con ayuda del for calculamos el total mensual y el promeio anual

--- Ejercicio1/Punto_24.java
+++ Ejercicio1/Punto_24.java
@@ -14,7 +14,20 @@ public class Punto_24 {
             }
         }
 
-        
+        int[] totalMes = new int [12];
+        for(int i = 0; i < 12; i++){
+            int suma = 0;
+            for(int j = 0; j < 4; j++){
+                suma += produccion[i][j];
+            }
+            totalMes[i] = suma;
+        }
+
+        int sumaAnual = 0;
+        for(int i = 0; i < 12; i++){
+            sumaAnual += totalMes[i];
+        }
+        double promedio = (double) sumaAnual / 12;
 
 
         
